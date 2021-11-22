bfc38e30b51dcf169f2800a8441229766e5d3a52
Oleksii <alexmatvichuk@gmail.com>
2021-11-22 11:31:55 +0100

    refactoring code

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 const contactsOperations = require("../../model/index");
 
+//
+const { contacts: ctrl } = require("../../controllers/index");
+//
+
 const contactsSchema = Joi.object({
   name: Joi.string().min(2).max(20).required(),
   email: Joi.string().min(3).max(30).required(),
