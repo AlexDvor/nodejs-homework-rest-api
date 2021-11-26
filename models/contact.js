4ebc7d4fb1c9d0a7044e70819ddfdef90666a366
Oleksii <alexmatvichuk@gmail.com>
2021-11-26 15:37:16 +0100

    add models

---
+++ models/contact.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const contactSchema = Shema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const JoiSchema = Joi.object({
+  name: Joi.string().min(2).max(20).required(),
+  email: Joi.string().min(3).max(30).required(),
+  phone: Joi.string().min(3).max(30).required(),
+});
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  JoiSchema,
+};
