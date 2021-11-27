9140440517d4055eaaf668553e753a5f26d2bc42
Oleksii <alexmatvichuk@gmail.com>
2021-11-27 20:12:49 +0100

    refactring code

--- models/contact.js
+++ models/contact.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-const Joi = require("joi");
 
 const contactSchema = Schema(
   {
@@ -21,20 +20,8 @@ const contactSchema = Schema(
   { versionKey: false, timestamps: true }
 );
 
-const JoiSchema = Joi.object({
-  name: Joi.string().min(2).max(20).required(),
-  email: Joi.string().min(3).max(30).required(),
-  phone: Joi.string().min(3).max(30).required(),
-});
-
-const statusJoiSchema = Joi.object({
-  favorite: Joi.boolean().required(),
-});
-
 const Contact = model("contact", contactSchema);
 
 module.exports = {
   Contact,
-  JoiSchema,
-  statusJoiSchema,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { contacts: ctrl } = require("../../controllers/index");
-const { contactsSchema } = require("../../schemas/index");
 const { validation } = require("../../middlewares/index");
-const { statusJoiSchema } = require("../../models/contact");
+const { contactsJoiSchema, statusJoiSchema } = require("../../schemas");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", validation(contactsSchema), ctrl.add);
+router.post("/", validation(contactsJoiSchema), ctrl.add);
 
 router.delete("/:contactId", ctrl.removeById);
 
-router.put("/:contactId", validation(contactsSchema), ctrl.updateById);
+router.put("/:contactId", validation(contactsJoiSchema), ctrl.updateById);
 
 router.patch(
   "/:contactId/favorite",
